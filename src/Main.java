314ace7c760aeda3f6ea1f4f545d0fed61dafd85
Avani14 <trivediavani14@gmail.com>
2022-06-02 08:38:52 +0530

    Adding contatct

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,13 @@
 
 public class Main {
-	static String first_name;
-	static String last_name;
-	static String address;
-	static String city;
-	static String state;
-	static String zip;
-	static long phone;
-	static String email_id;
+	
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Address Book!!!");
+		Contacts c = new Contacts();
+		c.contacts();
 	}
 
 }
+
